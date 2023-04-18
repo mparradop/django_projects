1d222ac42cc0964cc810d6517d9bac195b24f169
Mauricio Parrado P <mauricio.parrado@acm.org>
2023-04-18 00:56:48 -0400

    models main and crm created.

--- anothersaas/crm/models.py
+++ anothersaas/crm/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from main import models as core
 
 # Create your models here.
+
+class Competitor(models.Model):
+    id_competitor = models.AutoField(primary_key=True, db_index=True)
+    competitor_name = models.CharField(50,max_length=100)
+    competitor_code = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+
+class Account(models.Model):
+    id_account = models.AutoField(primary_key=True, db_index=True)
+    account_name = models.CharField(50,max_length=100)
+    sales_rep = models.ForeignKey(core.Employee, on_delete=models.CASCADE)
+    website = models.CharField(100,max_length=100)
+    industry = models.ForeignKey(core.Industry, on_delete=models.CASCADE)
+    status = models.ForeignKey(core.Status, on_delete=models.CASCADE)
+    region = models.ForeignKey(core.Region, on_delete=models.CASCADE)
+    territory = models.ForeignKey(core.Territory, on_delete=models.CASCADE)
+    country = models.ForeignKey(core.Country, on_delete=models.CASCADE)
+
+class Contact_Role(models.Model):
+    role_name = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+class Contact(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    name = models.CharField(40,max_length=100)
+    last_name = models.CharField(50,max_length=100)
+    role = models.ForeignKey(Contact_Role, on_delete=models.CASCADE)
+    email = models.CharField(50,max_length=100)
+    number = models.CharField(50,max_length=100)
+
+
+class Leads(models.Model):
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    source = models.ForeignKey(core.Source, on_delete=models.CASCADE)
+    persona = models.ForeignKey
+    product = models.ForeignKey(core.Product, on_delete=models.CASCADE)
+    detail = models.TextField(max_length=300)
+    status = models.ForeignKey(core.Status, on_delete=models.CASCADE)
+    next_step = models.TextField(max_length=300)
+    due_date = models.DateField()
+
--- anothersaas/main/models.py
+++ anothersaas/main/models.py
@@ -3,46 +3,100 @@ from django.db import models
 # Create your models here.
 
 class Country(models.Model):
-    id_country = models.AutoField(primary_key=True)
-    country_name = models.CharField(100)
-    CCTLD = models.CharField(20)
+    id_country = models.AutoField(primary_key=True, db_index=True)
+    country_name = models.CharField(100,max_length=100)
+    CCTLD = models.CharField(20,max_length=100)
+
 
 class Industry(models.Model):
-    industry_name = models.CharField(50)
+    industry_name = models.CharField(50,max_length=100)
+
 
 class Company(models.Model):
-    company_name = models.CharField(50)
-    domain = models.CharField(50)
-    address = models.CharField(100)
+    company_name = models.CharField(50,max_length=100)
+    domain = models.CharField(50,max_length=100)
+    address = models.CharField(100,max_length=100)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     industry = models.ForeignKey(Industry, on_delete=models.CASCADE)
 
 
+class Category(models.Model):
+    id_category = models.AutoField(primary_key=True, db_index=True)
+    category_name = models.CharField(50,max_length=100)
+    category_code = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+
+class Status(models.Model):
+    id_status = models.AutoField(primary_key=True, db_index=True)
+    status_name = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+class Flag(models.Model):
+    id_flag = models.AutoField(primary_key=True, db_index=True)
+    flag_name = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+class Source(models.Model):
+    id_source = models.AutoField(primary_key=True, db_index=True)
+    source_name = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+class Risk(models.Model):
+    id_risk = models.AutoField(primary_key=True, db_index=True)
+    risk_name = models.CharField(50,max_length=100)
+    risk_code = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
+class Priority(models.Model):
+    id_priority = models.AutoField(primary_key=True, db_index=True)
+    priority_name = models.CharField(50,max_length=100)
+    priority_code = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
 class Employee_Role(models.Model):
-    role_name = models.CharField(50)
-    description = models.CharField(100)
+    role_name = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
+
 
 class Employee(models.Model):
-    id_emp = models.AutoField(primary_key=True)
-    emp_name = models.CharField(40)
-    emp_last_name = models.CharField(50)
-    user_name = models.CharField(50)
+    id_emp = models.AutoField(primary_key=True, db_index=True)
+    emp_name = models.CharField(40,max_length=100)
+    emp_last_name = models.CharField(50,max_length=100)
+    user_name = models.CharField(50,max_length=100)
+    role = models.ForeignKey(Employee_Role, on_delete=models.CASCADE,default=0)
+
 
 class Org_team(models.Model):
-    id_org_team = models.AutoField(primary_key=True)
-    team_name = models.CharField(50)
-    description = models.CharField(100)
+    id_org_team = models.AutoField(primary_key=True, db_index=True)
+    team_name = models.CharField(50,max_length=100)
+    description = models.CharField(100,max_length=100)
 
-class Org_team_mngr(models.Model):
-    id_team_mngr = models.AutoField(primary_key=True)
+
+class Org_employee(models.Model):
     id_emp = models.ForeignKey(Employee, on_delete=models.CASCADE)
     id_org_team = models.ForeignKey(Org_team, on_delete=models.CASCADE)
 
+
+class Org_team_mngr(models.Model):
+    id_team_mngr = models.AutoField(primary_key=True, db_index=True)
+    id_emp = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    id_org_team = models.ForeignKey(Org_team, on_delete=models.CASCADE)
+
+
 class Region(models.Model):
-    region_name = models.Model(30)
+    region_name = models.CharField(30,max_length=100)
     director = models.ForeignKey(Employee, on_delete=models.CASCADE)
 
+
 class Territory(models.Model):
-    territory_name = models.Model(30)
+    territory_name = models.CharField(30,max_length=100)
     team_manager = models.ForeignKey(Org_team_mngr, on_delete=models.CASCADE)
 
+
+class Product(models.Model):
+    id_Product = models.AutoField(primary_key=True, db_index=True)
+    product_name = models.CharField(50,max_length=100)
+    product_code = models.CharField(50,max_length=100)
+    product_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    description = models.CharField(100,max_length=100)
